d9e95d126256931a8a5241fb4e4f54a658a639ab
Cosmas Augustino Billa <augubilla100@gmail.com>
2017-11-27 10:33:57 +0300

    Update shop.py
    The indentation was not correct and i also added line space "\n"

--- shop.py
+++ shop.py
@@ -1,19 +1,9 @@
-import os
-import sys
-
-
 shoppingList = []
 
 
 def instructions():
     print("What should we pick up at the shops?")
-    print("""
-You have {} items in your list.
-Enter HELP to show this help menu again.
-Enter SHOW to see your list.
-Enter REMOVE to remoeve an item.
-Enter CLEAR to delete your list.
-Enter SAVE to save your list and exit."""))
+    print("You have {} items in your list.\nEnter HELP to show this help menu again.\nEnter SHOW to see your list.\nEnter REMOVE to remoeve an item.\nEnter CLEAR to delete your list.\nEnter SAVE to save your list and exit.")
 
 
 def print_shopping_list():
@@ -27,9 +17,7 @@ def add_item_to_list(item):
     print("-------------------------------------------------------")
     print("Added {} to shopping list! List now has {} items".format(item, len(shoppingList)))
 
-
-
-def main():   
+def main():
     instructions()
     while True:
         item = str(input("Enter an item to add to the list... "))
@@ -42,5 +30,5 @@ def main():
             help_instruc()
         else:
             add_item_to_list(item)
-            
+
 main()
